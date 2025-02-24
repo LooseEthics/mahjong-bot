0198ded60d2cef7d9c1b7d71ca862fd372bf2fda
LooseEthics <36419655+LooseEthics@users.noreply.github.com>
2025-02-24 15:43:31 +0100

    Update common.py

--- common.py
+++ common.py
@@ -7,6 +7,7 @@ PON = 1
 CHII = 2
 MINKAN = 3
 SHOUMINKAN = 4
+Kans = (ANKAN, MINKAN, SHOUMINKAN)
 OMNames = {
     ANKAN: "Ankan",
     PON: "Pon",
@@ -17,8 +18,8 @@ OMNames = {
 WNames = ("East", "South", "West", "North")
 TenhouStr = "mpsz"
 
-def tilelist2tenhou(l):
-    l = sorted(l)
+def tilelist2tenhou(l, sort = True):
+    l = sorted(l) if sort else l
     s = ""
     csuit = -1
     nsuit = -1
@@ -65,41 +66,198 @@ class OpenMeld():
         else:
             s += tilelist2tenhou(4*[self.tile])
         return s
-        
+
+class Discard():
+    def __init__(self, turn, opid, tile):
+        self.turn = turn
+        self.owner_pid = opid
+        self.tile = tile
+    
+    def __iter__(self):
+        return iter((self.turn, self.owner_pid, self.tile))
+    
+    def __str__(self):
+        return f"Discard, Turn: {self.turn}, Owner: {WNames[self.owner_pid]}, Tile: {tilelist2tenhou([self.tile])}"
+   
 
 class GameState():
+    def __init__(self):
+        self.round_wind = 0
+        self.round_num = 0
+        self.honba = 0
+        self.scores = (25000, 25000, 25000, 25000)
+        self.init_round()
+    
+    def init_round(self):
+        self.round = RoundState()
+        self.players = [PlayerState(self.round, i) for i in range(4)]
+    
+    def __repr__(self):
+        s =  f"Game state: {WNames[self.round_wind]} {self.round_num}, Honba {self.honba}\n"
+        s += f"  Scores: {self.scores}"
+        return s
+    
+
+class RoundState():
     def __init__(self):
         self.random_start_state()
     
     def random_start_state(self):
+        self.turn = 0
+        self.active_player = 0
         self.tiles = [i for i in range(34) for _ in range(4)]
         shuffle(self.tiles)
-        self.hand_e = self.tiles[0:13].sort()
-        self.hand_s = self.tiles[13:26].sort()
-        self.hand_w = self.tiles[26:39].sort()
-        self.hand_n = self.tiles[39:52].sort()
-        self.hands = (self.hand_e, self.hand_s, self.hand_w, self.hand_n)
-        self.open = [] # tuple (owner pid, target pid, type, tile)
-        self.discard = [] # tuple (discarding pid, tile)
+        self.hands = [sorted(self.tiles[i*13: (i+1)*13]) for i in range(4)]
+        self.open = [] # OpenMeld instances
+        self.discard = [] # Discard instances
         self.riichi = [-1, -1, -1, -1] # -1 not in riichi, >=0 riichi declaration turn
         self.dead_wall = self.tiles[52:66] # dora 1-5, ura 1-5, kan draw 1-4
         self.live_wall = self.tiles[66:]
-        self.round_wind = randrange(4)
-        self.active_player = 0
-        self.turn = 0
-        self.kan_cnt = 0
-        
+        self.drawn_tile = -1
     
-class VisibleGameState():
-    def __init__(self, gs: GameState, pid: int):
+    def __repr__(self):
+        s =  f"Turn: {self.turn}\n"
+        s += f"Live wall: {tilelist2tenhou(self.live_wall, False)}\n"
+        s += f"Player hands: \n"
+        for i in range(4):
+            s += f"  {WNames[i].ljust(5)} - {tilelist2tenhou(self.hands[i])}\n"
+        s += f"Discards: {self.discard}\n"
+        s += f"Open melds: {self.open}\n"
+        s += f"Riichi: {self.riichi}\n"
+        s += f"Dora: {tilelist2tenhou(self.dead_wall[0:5], False)}\n"
+        s += f"Ura:  {tilelist2tenhou(self.dead_wall[5:10], False)}\n"
+        return s
+    
+    def kan_cnt(self):
+        return sum(1 for m in self.open if m.type in Kans)
+    
+    def last_discard(self):
+        return self.discard[-1].tile
+    
+    def who_can_pon(self):
+        ldt = self.discard[-1].tile
+        for pid in range(4):
+            if pid != self.active_player and self.hands[pid].count(ldt) >= 2:
+                return pid
+        return -1
+    
+    def who_can_chii(self):
+        ldt = self.discard[-1].tile
+        ldt_suit = ldt // 9
+        ldt_val = ldt % 9
+        hand = self.hands[self.active_player]
+        if ldt_suit < 3 and (
+            (ldt_val >= 2 and ldt - 1 in hand and ldt - 2 in hand) or
+            (ldt_val >= 1 and ldt_val <= 7 and ldt - 1 in hand and ldt + 1 in hand) or
+            (ldt_val <= 6 and ldt + 1 in hand and ldt + 2 in hand)
+        ):
+            return self.active_player
+        return -1
+    
+    def who_can_minkan(self):
+        ldt = self.discard[-1].tile
+        if self.kan_cnt < 4:
+            for pid in range(4):
+                if pid != self.active_player and self.hands[pid].count(ldt) == 3 :
+                    return pid
+        return -1
+    
+    def who_can_ankan(self):
+        if self.hands[self.active_player].count(self.drawn_tile) == 3 and self.kan_cnt < 4:
+            return self.active_player
+        return -1
+    
+    def who_can_shouminkan(self):
+        if self.kan_cnt < 4:
+            for m in self.open:
+                if m.owner_pid == self.active_player and m.type == PON and m.tile == self.drawn_tile:
+                    return self.active_player
+        return -1
+    
+    def who_can_riichi(self):
+        pass
+    
+    def action_draw(self):
+        self.drawn_tile = self.live_wall.pop(0)
+    
+    def action_draw_kan(self):
+        self.drawn_tile = self.dead_wall.pop(10)
+    
+    def action_discard(self, tile):
+        # tile id, not position in hand
+        self.hands[self.active_player].remove(tile)
+        self.hands[self.active_player].append(self.drawn_tile)
+        self.discard.append(Discard(self.turn, self.active_player, tile))
+        self.drawn_tile = -1
+        self.active_player = (self.active_player + 1) % 4
+        self.turn += 1
+    
+    def action_chii(self, start):
+        # starting tile
+        tile = self.last_discard()
+        pid = self.active_player
+        hand = self.hands[pid]
+        if start == tile:
+            hand.remove(start + 1)
+            hand.remove(start + 2)
+        elif start + 1 == tile:
+            hand.remove(start)
+            hand.remove(start + 1)
+        else:
+            hand.remove(start)
+            hand.remove(start + 2)
+        self.open.append(OpenMeld(pid, (pid - 1) % 4, CHII, start))
+    
+    def action_pon(self, pid):
+        tile = self.last_discard()
+        # twice
+        self.hands[pid].remove(tile)
+        self.hands[pid].remove(tile)
+        self.open.append(OpenMeld(pid, (self.active_player - 1) % 4, PON, tile))
+        self.active_player = pid
+    
+    def action_ankan(self):
+        tile = self.drawn_tile
+        pid = self.active_player
+        # thrice
+        self.hands[pid].remove(tile)
+        self.hands[pid].remove(tile)
+        self.hands[pid].remove(tile)
+        self.open.append(OpenMeld(pid, -1, ANKAN, tile))
+        self.drawn_tile = -1
+    
+    def action_minkan(self, pid):
+        tile = self.last_discard()
+        # thrice
+        self.hands[pid].remove(tile)
+        self.hands[pid].remove(tile)
+        self.hands[pid].remove(tile)
+        self.open.append(OpenMeld(pid, (self.active_player - 1) % 4, MINKAN, tile))
+        self.active_player = pid
+    
+    def action_shouminkan(self):
+        pass
+    
+    def action_riichi(self):
+        pass
+    
+    def action_ron(self):
+        pass
+    
+    def action_tsumo(self):
+        pass
+    
+    
+class PlayerState():
+    def __init__(self, rs: RoundState, pid: int):
         self.pid = pid
-        self.hand = gs.hands[pid]
-        self.open = gs.open
-        self.discard = gs.discard
-        self.visible_dora = gs.dead_wall[10: 11 + gs.kan_cnt]
-        
-            
+        self.rs = rs
+        self.hand = rs.hands[pid]
+        self.open = rs.open
+        self.discard = rs.discard
+        self.riichi = rs.riichi
 
-gs = GameState()
-vgs = VisibleGameState(gs, 0)
-print(tenhou2tilelist("234m234p"))
+
+g = GameState()
+g.init_round()
+print(g.round)
